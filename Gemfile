33d548e5cd1dbf12d9e840bc48f6fde2c780da1c
Uchio KONDO <udzura@udzura.jp>
2013-02-13 14:58:28 +0900

    Initial

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+# Specify your gem's dependencies in rack-post_body_msgpack_parser.gemspec
+gemspec
---
+++ lib/rack-post_body_msgpack_parser.rb
@@ -0,0 +1,7 @@
+require "rack-post_body_msgpack_parser/version"
+
+module Rack
+  module PostBodyMsgpackParser
+    # Your code goes here...
+  end
+end
